aa5575ee3aea59ebf253ade45ab9c631d545799c
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2022-10-15 22:14:54 +0200

    Bases

--- LorenaEcommerce/src/app/app.module.ts
+++ LorenaEcommerce/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { HomeComponent } from './home/pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ LorenaEcommerce/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const rutas: Routes = [];
+
+@NgModule({
+    imports: [RouterModule.forRoot(rutas)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
